f3c11bf5fa023dc559465c4b3d0465005de77446
Alex Christenson <christenson10@gmail.com>
2021-08-26 14:30:55 -0400

    20.1.6 navbar issue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import About from './components/About';
 
 function App() {
-
   return (
     <div>
+      <Navbar></Navbar>
       <main>
         <About></About>
       </main>
